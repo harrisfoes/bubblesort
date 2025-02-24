a2af7d31e1da0ce0a68eedf372714205d4eb2444
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-02-24 19:13:53 +0900

    clarify for readers

--- bubblesort.rb
+++ bubblesort.rb
@@ -3,7 +3,7 @@ def bubble_sort(array)
 
   while sorted_array != sorted_array.sort do
     sorted_array.each_with_index do | number, index |
-      if index + 1 < sorted_array.length
+      unless index + 1 == sorted_array.length
         if number > sorted_array[index+1] 
             sorted_array[index] = sorted_array[index+1]
             sorted_array[index+1] = number 
@@ -16,5 +16,8 @@ end
 
 array = [9,8,7,4,6,5,3,1]
 p bubble_sort(array)
+# => [1, 3, 4, 5, 6, 7, 8, 9]
 p bubble_sort([222,431,332,32,554,321,32])
+# => [32, 32, 222, 321, 332, 431, 554]
 p bubble_sort([4,3,78,2,0,2])
+# => [0, 2, 2, 3, 4, 78]
